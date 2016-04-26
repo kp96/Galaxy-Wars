65447f67a415cd5aa98314efff91b4e581fc4771
Krishna <krishnakalubandi@gmail.com>
2016-04-26 20:32:34 +0530

    added py2exe setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author: Krishna
+# @Date:   2016-04-26 20:18:23
+# @Last Modified by:   Krishna
+# @Last Modified time: 2016-04-26 20:23:18
+# -*- coding: utf-8 -*-
+# @Author: Krishna
+# @Date:   2016-04-26 20:18:23
+# @Last Modified by:   Krishna
+# @Last Modified time: 2016-04-26 20:18:23
+# -*- coding: utf-8 -*-
+# @Author: Krishna
+# @Date:   2016-04-26 20:18:23
+# @Last Modified by:   Krishna
+# @Last Modified time: 2016-04-26 20:18:23
+from distutils.core import setup
+import py2exe
+
+setup(console=['main.py'])
